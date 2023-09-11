1179efc9866be50154da5e849f4b7f3dd73a0772
Alexandre S <moutardea@gmail.com>
2023-09-11 22:31:16 +0200

    Fix moving items in backpack from other inventories

--- src/main/java/me/moutarde/realisticinventory/mixin/ScreenHandlerMixin.java
+++ src/main/java/me/moutarde/realisticinventory/mixin/ScreenHandlerMixin.java
@@ -1,2 +1,25 @@
-package me.moutarde.realisticinventory.mixin;public class ScreenHandlerMixin {
+package me.moutarde.realisticinventory.mixin;
+
+import me.moutarde.realisticinventory.items.BackpackItem;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ScreenHandler.class)
+public class ScreenHandlerMixin {
+    @Inject(method = "onClosed", at = @At("TAIL"))
+    private void injected(PlayerEntity player, CallbackInfo ci)
+    {
+        if (player instanceof ServerPlayerEntity && player.realistic_inventory$hasBackpack()) {
+            BackpackItem.saveBackpack(
+                    player.getEquippedStack(EquipmentSlot.CHEST),
+                    player.getInventory().main.subList(player.realistic_inventory$getHotbarSlots(), player.realistic_inventory$getHotbarSlots() + 9)
+            );
+        }
+    }
 }
