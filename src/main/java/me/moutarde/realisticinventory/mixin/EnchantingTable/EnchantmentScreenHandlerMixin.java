629ce7bb14a717056913b50098ae052407f50560
Alexandre S <moutardea@gmail.com>
2023-09-20 21:13:00 +0200

    Fix chest & enchantment table inventories

--- src/main/java/me/moutarde/realisticinventory/mixin/EnchantingTable/EnchantmentScreenHandlerMixin.java
+++ src/main/java/me/moutarde/realisticinventory/mixin/EnchantingTable/EnchantmentScreenHandlerMixin.java
@@ -17,49 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EnchantmentScreenHandler.class)
 public abstract class EnchantmentScreenHandlerMixin {
-    @Shadow
-    @Final
-    private Inventory inventory;
-
-    @Redirect(method = "<init>(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/screen/ScreenHandlerContext;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/EnchantmentScreenHandler;addSlot(Lnet/minecraft/screen/slot/Slot;)Lnet/minecraft/screen/slot/Slot;"))
-    public Slot onAddSlot(EnchantmentScreenHandler instance, Slot slot) {
-        return slot;
-    }
-
-    @Inject(method = "<init>(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/screen/ScreenHandlerContext;)V", at = @At(value = "TAIL"))
-    public void injected(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context, CallbackInfo ci) {
-        int i;
-
-        ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(this.inventory, 0, 15, 47){
-
-            @Override
-            public boolean canInsert(ItemStack stack) {
-                return true;
-            }
-
-            @Override
-            public int getMaxItemCount() {
-                return 1;
-            }
-        });
-        ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(this.inventory, 1, 35, 47){
-
-            @Override
-            public boolean canInsert(ItemStack stack) {
-                return stack.isOf(Items.LAPIS_LAZULI);
-            }
-        });
-
-        for(i = 0; i < playerInventory.player.realistic_inventory$getInventorySlots(); ++i) {
-            ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(playerInventory, playerInventory.player.realistic_inventory$getHotbarSlots() + i, 8 + (i%9) * 18, 84 + (i/9) * 18));
-        }
-
-        final int offset = 18 * 4;
-        for (i = 0; i < playerInventory.player.realistic_inventory$getHotbarSlots(); ++i) {
-            ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(playerInventory, i, offset + 8 + i * 18, 142));
-        }
-    }
-
     @ModifyConstant(method = "quickMove", constant = @Constant(intValue = 38))
     public int injected2(int value, PlayerEntity player) {
         return 2 + player.realistic_inventory$getInventorySlots() + player.realistic_inventory$getHotbarSlots();
--- src/main/java/me/moutarde/realisticinventory/mixin/Generic3x3Container/Generic3x3ContainerScreenHandlerMixin.java
+++ src/main/java/me/moutarde/realisticinventory/mixin/Generic3x3Container/Generic3x3ContainerScreenHandlerMixin.java
@@ -16,32 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class Generic3x3ContainerScreenHandlerMixin {
     @Shadow @Final private static int INVENTORY_START;
 
-    @Redirect(method = "<init>(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/inventory/Inventory;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Generic3x3ContainerScreenHandler;addSlot(Lnet/minecraft/screen/slot/Slot;)Lnet/minecraft/screen/slot/Slot;"))
-    public Slot onAddSlot(Generic3x3ContainerScreenHandler instance, Slot slot) {
-        return slot;
-    }
-
-    @Inject(method = "<init>(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/inventory/Inventory;)V", at = @At(value = "TAIL"))
-    public void injected(int syncId, PlayerInventory playerInventory, Inventory inventory, CallbackInfo ci) {
-        int j;
-        int i;
-
-        for (i = 0; i < 3; ++i) {
-            for (j = 0; j < 3; ++j) {
-                ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(inventory, j + i * 3, 62 + j * 18, 17 + i * 18));
-            }
-        }
-
-        for(i = 0; i < playerInventory.player.realistic_inventory$getInventorySlots(); ++i) {
-            ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(playerInventory, playerInventory.player.realistic_inventory$getHotbarSlots() + i, 8 + (i%9) * 18, 84 + (i/9) * 18));
-        }
-
-        final int offset = 18 * 4;
-        for (i = 0; i < playerInventory.player.realistic_inventory$getHotbarSlots(); ++i) {
-            ((ScreenHandlerInvoker) this).invokeAddSlot(new Slot(playerInventory, i, offset + 8 + i * 18, 142));
-        }
-    }
-
     @ModifyConstant(method = "quickMove", constant = @Constant(intValue = 45))
     private int injected2(int value, PlayerEntity player) {
         return INVENTORY_START + player.realistic_inventory$getInventorySlots() + player.realistic_inventory$getHotbarSlots();
