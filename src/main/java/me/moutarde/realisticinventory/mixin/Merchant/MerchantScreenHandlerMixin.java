8f70d6b38515ddcb94bbf82e8986680bb9461762
Alexandre S <moutardea@gmail.com>
2023-09-26 23:17:52 +0200

    Fix villager trade crashing the game

--- src/main/java/me/moutarde/realisticinventory/mixin/Merchant/MerchantScreenHandlerMixin.java
+++ src/main/java/me/moutarde/realisticinventory/mixin/Merchant/MerchantScreenHandlerMixin.java
@@ -1,16 +1,27 @@
 package me.moutarde.realisticinventory.mixin.Merchant;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.MerchantScreenHandler;
+import net.minecraft.village.Merchant;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MerchantScreenHandler.class)
 public class MerchantScreenHandlerMixin {
     @Unique
     PlayerEntity player;
+
+    @Inject(method = "<init>(ILnet/minecraft/entity/player/PlayerInventory;Lnet/minecraft/village/Merchant;)V", at = @At("TAIL"))
+    private void injectPlayer(int syncId, PlayerInventory playerInventory, Merchant merchant, CallbackInfo ci)
+    {
+        this.player = playerInventory.player;
+    }
 
     @ModifyConstant(method = "switchTo", constant = @Constant(intValue = 39))
     public int injectedConstant3(int value) {
